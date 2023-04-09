4301ec15fd961e08d75b4f523f5dd3a6c3b4fd19
Steven Lalewicz <steven.lalewicz@gmail.com>
2023-04-09 23:28:06 +0100

    Add display of simple stats to client.

--- client.py
+++ client.py
@@ -2,6 +2,18 @@ import asyncio
 import json
 import sys
 
+def get_temp(data):
+    return data[1][0]
+
+def get_press(data):
+    return data[1][1]
+
+def get_humid(data):
+    return data[1][2]
+
+def get_min_max(data, fn):
+    return fn(min(data, key=fn, default=0)), fn(max(data, key=fn, default=0))
+
 async def temp_client(message, address, port): 
      print(f'Send:{message!r}')
      reader, writer = await asyncio.open_connection(address, port)
@@ -25,6 +37,11 @@ async def main(argv):
      message = "GETTEMP\n"
      data = await temp_client(message, address, port)
      x = json.loads(data)
-     print(x)
+     min_value, max_value = get_min_max(x, get_temp) 
+     print(f'Temp - min: {min_value} max: {max_value}')
+     min_value, max_value = get_min_max(x, get_press) 
+     print(f'Pres - min: {min_value} max: {max_value}')
+     min_value, max_value = get_min_max(x, get_humid) 
+     print(f'Humi - min: {min_value} max: {max_value}')
 
 asyncio.run(main(sys.argv))
